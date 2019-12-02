5cb6a1d61e0bb45910ada2640ec2b527d0d659cf
z-t-y <44891657+z-t-y@users.noreply.github.com>
2019-12-02 15:18:17 +0800

    Allowing Continuous Movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -36,7 +37,11 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
